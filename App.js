7b71a6d1b0eef61a37b3673559957753c7a3c1d7
Baraka-Mugisha <mugishaje@gmail.com>
2022-02-26 23:58:59 +0200

    building the icon component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View } from 'react-native';
 import AppButton from './app/components/AppButton';
 import AppText from './app/components/AppText';
 import Card from './app/components/Card';
+import Icon from './app/components/Icon';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -9,6 +10,11 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    // <MessagesScreen />
+    <Icon
+      name="email"
+      size={50}
+      backgroundColor="red"
+      iconColor="white" />
   );
 }
