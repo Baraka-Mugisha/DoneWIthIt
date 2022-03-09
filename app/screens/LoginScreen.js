2f511ac4e69d76fdb8bc4da006ea77bff0927f53
Baraka-Mugisha <mugishaje@gmail.com>
2022-03-09 23:41:50 +0200

    2 - building the login form

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import Screen from '../components/Screen';
 
-function LoginScreen(props) {
+function LoginScreen({ props }) {
   return (
-    <div>
-      
-    </div>
+    <Screen>
+      <Image source={require("../assets/logo-red.png")} style={StyleSheet.logo} />
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 80,
+    alignContent: 'center',
+    marginTop: 50,
+    marginBottom: 20
+  }
+});
 
 export default LoginScreen;
